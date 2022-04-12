9109a16241e49a6cd549e3390af752593ceddcf8
Nancy Chaudhry <nancych14@gmail.com>
2022-04-12 08:44:38 +0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Unsplash access key tnrXgxBLnfICUXsbKZ1fTIPl0e0CrLa5th21GJoqJNE
+// Unsplash secret key UQWCfNbv7wRTFOtE1E8mcBEGZwuf8UVCyASfTcy4AQo
+
+
+document.querySelector('button').addEventListener('click', getFunFact)
+
+function getFunFact() {
+  const urlFacts = `https://api.aakhilv.me/fun/facts`
+  const urlPhotos = `https://api.unsplash.com/photos/random?client_id=tnrXgxBLnfICUXsbKZ1fTIPl0e0CrLa5th21GJoqJNE`
+
+  let fact = document.querySelector('.fact')
+  let photo = document.querySelector('img')
+
+  fetch(urlFacts)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      fact.innerText = data[0] 
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    })
+
+  fetch(urlPhotos)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+      photo.src = data.urls.thumb
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    })
+}
